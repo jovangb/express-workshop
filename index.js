fc554b3640b98c8cbfcbf1a1e7cceb56a4c8490c
Jovan <gbjovan@gmail.com>
2021-03-18 23:04:26 -0600

    Actividad 6

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
+const bodyParser = require('body-parser'); 
 const express = require('express');
 const app = express();
 const {pokemon} = require('./pokedex.json');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get("/", (req, res, next) => {
-    res.status(200);
-    res.send("Bienvenido al Pokedex");
+    return res.status(200).send("Bienvenido al Pokedex");
+});
+
+app.post("/pokemon",(req,res,next) => {
+    return res.status(200).send(req.body.name);
 });
 
 app.get("/pokemon",(req, res, next) =>{
-    res.status(200);
-    res.send(pokemon);
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1,3})',(req, res, next) =>{
     const id = req.params.id - 1;
     if(id >= 0 && id <= 150){
-        res.status(200);
-        res.send(pokemon[req.params.id - 1])
+        return res.status(200).send(pokemon[req.params.id - 1])
     }else{
-        res.status(404);
-        res.send("Pokemón no encontrado");
+        return res.status(404).send("Pokemón no encontrado");
     }
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
 });
 
-app.get('/pokemon/:name',(req,res,next) => {
+app.get('/pokemon/:name([A-Za-z]+)',(req,res,next) => {
     const name = req.params.name;
-    for (i = 0; i < pokemon.length; i++){
-        if(pokemon[i].name == name){
-            res.status(200);
-            res.send(pokemon[i]);
-        }
+    const pk = pokemon.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) ? p : null;
+    });
+
+    if(pk.length > 0){
+        return res.status(200).send(pk);
     }
-    res.status(404);
-    res.send('Pokemon no encontrado')
+    return res.status(404).send('Pokemon no encontrado')
 });
 
 app.listen(process.env.PORT || 3000, () => {
