bb0ba1be694a7eec9382c90370f47830aa16bf99
Jovan <gbjovan@gmail.com>
2021-03-23 21:40:38 -0600

    Actividad 7

---
+++ routes/pokemon.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const pokemon = express.Router();
+const db = require('../config/database');
+
+pokemon.post("/",(req,res,next) => {
+    return res.status(200).send(req.body.name);
+});
+
+pokemon.get("/",async (req, res, next) =>{
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    console.log(pkmn);
+    return res.status(200).json(pkmn);
+});
+
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) =>{
+    const id = req.params.id;
+    if(id >= 1 && id <= 722){
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = "+id);
+        console.log(pkmn);
+        return res.status(200).json(pkmn)
+    }else{
+        return res.status(404).send("Pokemón no encontrado");
+    }
+});
+
+pokemon.get('/:name([A-Za-z]+)', async (req,res,next) => {
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '"+name.toLowerCase()+"'");
+    console.log(pkmn);
+
+    if(pkmn.length > 0){
+        return res.status(200).json(pkmn);
+    }else{
+        return res.status(200).send('Pokemon no encontrado');
+    }
+});
+
+module.exports = pokemon;
